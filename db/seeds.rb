8a50d890444d8afc538f2691b483c60a6e2570b3
Edgar Minault <e.minault@bluewin.ch>
2020-05-18 18:00:33 +0200

    Seeds created.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-
+puts 'Creating 20 fake  users...'
+20.times do
+  full_name = Faker::Name.name
+  p full_name
+  surname = full_name.split[0].downcase
+  last_name = full_name.split[1].downcase
+  email = "#{surname}.#{last_name}@loremipsum.com"
+  p email
+  address = "Le Mans"
+  p address
+  description = Faker::Lorem.paragraph(sentence_count: 2)
+  p description
+  # user = User.new(
+  #   name: full_name,
+  # )
+  # user.save!
+  puts 'User added.'
+end
+puts 'All 20 users added.'

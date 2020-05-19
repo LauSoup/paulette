80ae7acb6e9f746f84241f3da7d160044cce2a12
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-19 12:49:01 +0200

    add punding on users show + global config

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem 'pundit'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    authorize @user
   end
 
 end

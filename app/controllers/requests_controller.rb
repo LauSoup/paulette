08271e4563dc6edcd8c88c1a8cc6dd38fca5e9be
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-19 10:01:19 +0200

    saving request works

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,15 +1,20 @@
 class RequestsController < ApplicationController
 
   def new
-      @hen = Hen.find(params[:hen_id])
+      @hen = Hen.find(params[:hen_id]) # rescue nil
       # @user = User.find(params[:user_id])
       @request = Request.new
     end
 
     def create
+
       @request = Request.new(requests_params)
+      # @request.hen = current_user
       @hen = Hen.find(params[:hen_id])
       @request.hen = @hen
+      @request.user = @hen.user
+
+      @request.save
       if @request.save
         redirect_to dashboard_path
       else
@@ -20,6 +25,6 @@ class RequestsController < ApplicationController
     private
 
     def requests_params
-      params.require(:request).permit(:duration, :hen_id, :user_id)
+      params.require(:request).permit(:duration)
     end
 end

7a2bbee45651fb7d830f763dde4c01d01e8163a6
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-19 10:29:36 +0200

    updated controller resquest

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,15 @@
 class RequestsController < ApplicationController
 
   def new
-      @hen = Hen.find(params[:hen_id]) # rescue nil
-      # @user = User.find(params[:user_id])
+      @hen = Hen.find(params[:hen_id])
       @request = Request.new
     end
 
     def create
-
       @request = Request.new(requests_params)
-      # @request.hen = current_user
+      @request.user = current_user
       @hen = Hen.find(params[:hen_id])
       @request.hen = @hen
-      @request.user = @hen.user
-
       @request.save
       if @request.save
         redirect_to dashboard_path

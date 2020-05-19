4757249db9b89d1d79e721f3028abde94e7724b5
Laurence Mouren <laurence.mouren@gmail.com>
2020-05-19 08:47:07 +0200

    refacto method index users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    # @users = User.all
     users_initial = User.all
-    @users = users_initial.reject do |user|
-      user.hens.empty?
-    end
-  @users
+    @users = users_initial.reject { |user| user.hens.empty? }
   end
 
   def show

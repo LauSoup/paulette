48f48a17345e7202f52b85688059c2f7a9b98241
Edgar Minault <e.minault@bluewin.ch>
2020-05-20 14:21:26 +0200

    New form done. validation ok.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,12 @@
 class Request < ApplicationRecord
   belongs_to :user
   belongs_to :hen
+  validate :ending_is_possible?
+
+  def ending_is_possible?
+    return if [ending.blank?, beginning.blank?].any?
+    if beginning > ending
+      errors.add(:ending, 'must be possible')
+    end
+  end
 end

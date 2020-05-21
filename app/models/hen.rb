2b59b9d47612ce0ba7334317f9a753fb20709c06
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-21 16:23:45 +0200

    reviews on user card

--- app/models/hen.rb
+++ app/models/hen.rb
@@ -1,12 +1,8 @@
 class Hen < ApplicationRecord
   belongs_to :user
   has_many :requests
-<<<<<<< HEAD
   has_many :reviews
-=======
   has_one_attached :photo
-
->>>>>>> 78322183d3b8d9cac8faa47667ed29b894911b07
   validates :name, presence: true
   validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_05_21_103017) do
-=======
-
 ActiveRecord::Schema.define(version: 2020_05_21_105347) do
-
->>>>>>> 78322183d3b8d9cac8faa47667ed29b894911b07
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

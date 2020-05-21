2fdbcd04fca8d4066df41df76029e00e4b03707a
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-21 11:52:31 +0200

    calendar done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,9 @@ require("channels")
 
 import flatpickr from 'flatpickr'
 import 'flatpickr/dist/flatpickr.min.css'
-
 flatpickr(".datepicker", {
-  altInput: true
 });
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

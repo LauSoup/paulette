494297f276785ef8d06864164b44450517015ff2
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-21 11:16:19 +0200

    calendar's working on new request

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import flatpickr from 'flatpickr'
+import 'flatpickr/dist/flatpickr.min.css'
 
+flatpickr(".datepicker", {
+  altInput: true
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)

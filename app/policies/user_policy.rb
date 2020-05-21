38e12626b1c990e8313c338f6bfdfaa6fb0fd0b9
Noeline Horta <noeline.boyer@gmail.com>
2020-05-21 12:36:38 +0200

    geocoding & search in map & markers

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,7 +2,7 @@ class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       # scope.all
-      users_initial = User.all
+      users_initial = User.all.geocoded
       @users = users_initial.reject { |user| user.hens.empty? }
     end
   end
